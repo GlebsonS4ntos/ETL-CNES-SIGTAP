c117fce6cba870925146f8a78b007505a199791e
Glebson Santos <glebsonsantos@outlook.pt>
2025-08-21 16:35:42 -0300

    feat: Add parallelFlow do job

--- src/main/java/com/Glebson/ETL/Config/BatchConfig.java
+++ src/main/java/com/Glebson/ETL/Config/BatchConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,26 +13,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
+@EnableBatchProcessing
 public class BatchConfig {
+
     private final PlatformTransactionManager platformTransactionManager;
     private final JobRepository jobRepository;
+    private final FlowConfig flowConfig;
 
-    public BatchConfig(PlatformTransactionManager platformTransactionManager, JobRepository jobRepository) {
+    public BatchConfig(PlatformTransactionManager platformTransactionManager,
+                       JobRepository jobRepository,
+                       FlowConfig flowConfig) {
         this.platformTransactionManager = platformTransactionManager;
         this.jobRepository = jobRepository;
+        this.flowConfig = flowConfig;
     }
 
     @Bean
-    Job job(DownloadUnzipTasklet downloadUnzipTasklet){
+    public Job job(DownloadUnzipTasklet downloadUnzipTasklet) {
+        Flow parallelFlow = flowConfig.processarArquivosParaleloFlow(jobRepository, platformTransactionManager);
+
+        Step flowStep = new StepBuilder("processarArquivosParaleloFlowStep", jobRepository)
+                .flow(parallelFlow)
+                .build();
+
         return new JobBuilder("job", jobRepository)
                 .start(downloadStep(downloadUnzipTasklet))
+                .next(flowStep)
                 .build();
     }
 
     @Bean
-    public Step downloadStep(DownloadUnzipTasklet downloadUnzipStep){
+    public Step downloadStep(DownloadUnzipTasklet downloadUnzipTasklet) {
         return new StepBuilder("downloadStep", jobRepository)
-                .tasklet(downloadUnzipStep, platformTransactionManager)
+                .tasklet(downloadUnzipTasklet, platformTransactionManager)
                 .build();
     }
 }
