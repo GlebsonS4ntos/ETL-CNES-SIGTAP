2c33b1489d66adcb52ab6ddec3fda0c5a82c52de
Glebson Santos <glebsonsantos@outlook.pt>
2025-09-01 00:10:15 -0300

    feat: add Estabelecimento Record

---
+++ src/main/java/com/Glebson/ETL/Domain/Cnes/Estabelecimento.java
@@ -0,0 +1,8 @@
+package com.Glebson.ETL.Domain.Cnes;
+
+public record Estabelecimento(
+        String coEstabelecimento,
+        String cnes,
+        String noEstabelecimento,
+        String coMunicipio,
+        String coEstado) { }
